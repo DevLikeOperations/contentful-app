ecdf1d8eab8bccce9f6babf7443074784f4ffe96
nchoi <nicolas_choi@brown.edu>
2017-06-22 17:04:59 -0400

    removed showdown and added marked

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 
 var contentful = require("contentful");
-var showdown = require("showdown");
 var express = require("express");
 var logger = require("morgan");
 var app = express();
 var keys = require('./keys');
-var cors = require('cors');
+var marked = require('marked');
 
 //Logs things in console
 app.use(logger('short'));
-
-app.use(cors());
 
 //Contentful's own http module
 var client = contentful.createClient({
@@ -19,8 +16,10 @@ var client = contentful.createClient({
 });
 
 //Third party parses MD to HTML
-var converter = new showdown.Converter();
-
+marked.setOptions({
+  gfm: true,
+  breaks: true,
+});
 
 //Maps our contentID to a chapterNumber for our route unless we want to just use contentID
 var TableOfContents = {
@@ -28,27 +27,28 @@ var TableOfContents = {
 	'1': '',
 	'2': '',
 	'3': '',
-	'4': '',
+	'4': '2lEZ1ZEq6k2E2swi4OseCa',
 	'5': '',
 	'6': '',
 };
 
-
-
 var get_chapter = (chapter_number) =>
 {
-	return client.getEntries({'sys.id': TableOfContents.TESTING}).then(function(res){
+	return client.getEntries({'sys.id': TableOfContents[chapter_number]}).then(function(res){
 		var info = res.items[0].fields;
 		var title = info.title;
 		var body = info.body;
-
-		var bodyHTML = converter.makeHtml(body);
-
+		var bodyHTML = marked(body); 
 		return bodyHTML;	
 	}).catch(function(e){
 		return e;
 	});
 };
+
+app.get('/', function(req, res, next){
+    res.sendFile('./index.html', { root: __dirname });
+
+});
 
 app.get('/:num', function(req, res, next){
 	var chapter_number = req.params.num;
