a4ec165464470f93b57f452544a35118a11e5425
Mariya B <mb2283@cornell.edu>
2017-07-06 11:01:56 -0400

    hello route added

--- keys.js
+++ keys.js
@@ -1,14 +1,6 @@
-const SPACE_ID = 'iqe9w3ezvrf8';
-const DELIVERY = 'a4ad8d84d679a4692076986bb9bfac1311ebfee7061999f4c731a2d6c2982308';
-const MANAGEMENT = 'CFPAT-071b3bd47fdf32890130fed525fd8af2474de273d9de6ebf300c70bd225a7393';
-
-const SECRET = 'bcff146c66bebb1b523d3dc9dc5c7bbbef921b53b6b613fc3a0aa175adb557dd'
-
 module.exports = {
-
-
-  spaceid: SPACE_ID,
-  delivery_key: DELIVERY,
-  management: MANAGEMENT,
-  secret: SECRET
+  spaceid: process.env.SPACE_ID,
+  delivery_key: process.env.DELIVERY,
+  management: process.env.MANAGEMENT,
+  secret: process.env.SECRET
 };
