f8039bf5a60c9d444c66ed98c0d4a88f505c2611
nchoi <nicolas_choi@brown.edu>
2017-07-18 16:39:41 -0400

    spacing issue

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.get('/:id', function (req, res, next) {
 	const baseReferer = referer.match(regEx) ? referer.match(regEx)[0] : null;
 
 	if (ALLOWED_BY.has(baseReferer)){
-    	res.setHeader('X-Frame-Options', 'ALLOW-FROM ' + baseReferer);
+		res.setHeader('X-Frame-Options', 'ALLOW-FROM ' + baseReferer);
 		res.sendFile(path.join(__dirname, 'build', 'index.html'));
 	}else{
 		next();
