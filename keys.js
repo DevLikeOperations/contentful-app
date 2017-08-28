e0efb4aeac0507ef3895780cfdee9e75cdddfa94
Mariya B <mb2283@cornell.edu>
2017-08-28 13:47:27 -0400

    clean

--- keys.js
+++ keys.js
@@ -1,13 +1,9 @@
-process.env.TEXTBOOK_SPACE_ID = 'iqe9w3ezvrf8';
-process.env.TEXTBOOK_DELIVERY = '4d255b750f805f4016209c022205f69e09927d215eb19e8bcff6947fe5b8e2c7';
-process.env.TEXTBOOK_ID = '5LrkCNNUVqgY40ssIO0GuY';
-process.env.COMMUNITY_SPACE_ID = 'qdcs9egjk9fy';
-process.env.COMMUNITY_DELIVERY = 'b9d7a99f6b191d85e7640d12b1535df7e9f54a06e99ab1f51e17b748badfd1b6';
-
 module.exports = {
   textbook_space_id: process.env.TEXTBOOK_SPACE_ID,
   textbook_delivery_key: process.env.TEXTBOOK_DELIVERY,
   textbook_id: process.env.TEXTBOOK_ID,
   community_space_id: process.env.COMMUNITY_SPACE_ID,
   community_delivery_key: process.env.COMMUNITY_DELIVERY,
+  wellness_delivery_key: process.env.WELLNESS_DELIVERY,
+  wellness_id: process.env.WELLNESS_ID
 };
