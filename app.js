6f35fbc4ecd9b6f3956b7bff58ab0eed5b6ddbc5
Keith Morris <keith@crisistextline.org>
2017-09-01 11:20:32 -0400

    Cleanup

--- app.js
+++ app.js
@@ -6,33 +6,20 @@ const path = require('path');
 
 const app = express();
 
-
 var textbook = require('./routes/textbook');
 var community = require('./routes/community');
 var api = require('./routes/api');
 
-//Logs things in console
 app.use(logger('short'));
-
-
-
 app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/hello', function (req, res, next) {
-	res.send('Hello');
-});
-
-
 app.use('/community', community);
 app.use('/api', api);
 app.use('*', textbook);
 
+app.get('/hello', function (req, res, next) {
+    res.send('Hello');
+});
 
 app.listen(8081, function(){
 	console.log('Listening on route 8081!');
 });
-
-
-
-
-
